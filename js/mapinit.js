2768627920be937022df0db8b14399780f81c1d3
Alex <alexaca79@gmail.com>
2016-08-10 13:45:45 -0400

    Fixed  Popups and added Clustering for Program Layer

--- js/mapinit.js
+++ js/mapinit.js
@@ -13,8 +13,11 @@ var map = new mapboxgl.Map( {
 map.on( 'load', function() {
   map.addSource( "places", {
       "type": "geojson",
-      "data": "data/convertcsv.geojson"
-
+      "data": "data/convertcsv.geojson",
+      "tolerance": 0.00000001,
+      cluster: true,
+      clusterMaxZoom: 14, // Max zoom to cluster points on
+      clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
   } );
 
     places.features.forEach( function( feature ) {
@@ -33,6 +36,8 @@ map.on( 'load', function() {
         },
         "filter": ["==", "FundProgram", symbol]
       });
+
+
 
 
       // Add checkbox and label elements for the layer.
@@ -55,12 +60,54 @@ map.on( 'load', function() {
 
       });
 
+        var layers = [
+            [150, '#f28cb1'],
+            [20, '#f1f075'],
+            [0, '#51bbd6']
+        ];
+
+        layers.forEach(function (layer, i) {
+            map.addLayer({
+                "id": layerID + i,
+                "type": "circle",
+                "source": "places",
+                "paint": {
+                    "circle-color": layer[1],
+                    "circle-radius": 18
+                },
+                "filter": i === 0 ?
+                    [">=", "point_count", layer[0]] :
+                    ["all",
+                        [">=", "point_count", layer[0]],
+                        ["<", "point_count", layers[i - 1][0]]]
+            });
+        });
+
+        // Add a layer for the clusters' count labels
+        map.addLayer({
+            "id": "cluster-count",
+            "type": "symbol",
+            "source": "places",
+            "layout": {
+                "text-field": "{point_count}",
+                "text-font": [
+                    "DIN Offc Pro Medium",
+                    "Arial Unicode MS Bold"
+                ],
+                "text-size": 12
+            }
+        });
+
 
       map.on('click', function (e) {
         var features = map.queryRenderedFeatures(e.point, {
-          layers: ['layerID']
+          layers: [layerID]
         });
         var feature = features[0];
+
+          if (!features.length) {
+              return;
+          }
 
         var popup = new mapboxgl.Popup({
           anchor: 'top'
@@ -77,8 +124,11 @@ map.on( 'load', function() {
 
       map.on('mousemove', function (e) {
         var features = map.queryRenderedFeatures(e.point, {
-          layers: ['layerID']
+          layers: [layerID]
         });
+          if (!features.length) {
+              return;
+          }
         map.getCanvas().style.cursor = ( features.length ) ? 'pointer' : '';
 
       });
