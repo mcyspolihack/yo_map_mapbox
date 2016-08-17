4269eca5f4ff342aa7e687eccae27c34fab87417
Alex <alexaca79@gmail.com>
2016-08-17 15:26:51 -0400

    Added Colour

--- js/mapinit.js
+++ js/mapinit.js
@@ -1,3 +1,24 @@
+$(document).ready(function() {
+    if($(".splash").is(":visible")) {
+        $(".wrapper").css({"opacity":"0"});
+    }
+
+    $(".splash-arrow").click(function() {
+        $(".splash").slideUp("800", function() {
+            $(".wrapper").delay(100).animate({"opacity":"1.0"},800);
+        });
+    });
+});
+
+$(window).scroll(function() {
+    $(window).off("scroll");
+    $(".splash").slideUp("800", function() {
+        $("html, body").animate({"scrollTop":"0px"},100);
+        $(".wrapper").delay(100).animate({"opacity":"1.0"},800);
+    });
+});
+
+
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWxleGFjYTc5IiwiYSI6ImNpbzYyZGVlNzAyNjd2d2x6dHY1MnR6MjgifQ.anutU5yQ38NCFEMAM4Ubdw';
 
 var filterGroup = document.getElementById( 'filter-group' );
@@ -10,11 +31,16 @@ var map = new mapboxgl.Map( {
   zoom: 14 // starting zoom
 } );
 
+var layers = [
+    [150, '#f28cb1'],
+    [40, '#f1f075'],
+    [0, '#51bbd6']
+];
+
 map.on( 'load', function() {
   map.addSource( "places", {
       "type": "geojson",
       "data": "data/convertcsv.geojson",
-      "tolerance": 0.00000001,
       cluster: true,
       clusterMaxZoom: 14, // Max zoom to cluster points on
       clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
@@ -28,17 +54,81 @@ map.on( 'load', function() {
 
       map.addLayer({
         "id": layerID,
-        "type": "symbol",
+        "type": "circle",
         "source": "places",
         "layout": {
-          "icon-image": "circle-11",
-          'visibility': 'visible'
+            'visibility': 'visible',
+        },
+        "paint": {
+            'circle-radius': {
+                'base': 5,
+                'stops': [[7, 3], [12, 5]]
+
+        },
+            "circle-color": {
+                property: 'FundProgram',
+                type: 'categorical',
+                stops:[
+                    ['YOF', '#fbb03b'],
+                    ['GPIP', '#4F81BD'],
+                    ['YMP', '#9BBB59'],
+                    ['RJCM', '#8064A2'],
+                    ['SAYIPI', '#F79646'],
+                    ['SYIPI', '#F79646'],
+                    ['AYIPI', '#F79646'],
+                    ['SNAP', '#95B3D7'],
+                    ['YOW', '#4BACC6']
+
+
+
+                ]
+
+            },
+
+
+
+
+
         },
         "filter": ["==", "FundProgram", symbol]
+
       });
 
+      layers.forEach(function (layer, i) {
+            map.addLayer({
+                "id": layerID + i,
+                "type": "circle",
+                "source": "places",
+                "paint": {
+                    "circle-color": layer[1],
+                    "circle-radius": 18
+                },
+                "filter": i === 0 ?
+                    [">=", "point_count", layer[0]] :
+                        ["all",
+                        [">=", "point_count", layer[0]],
+                        ["<", "point_count", layers[i - 1][0]]]
+            });
+        });
 
+        // Add a layer for the clusters' count labels
+        map.addLayer({
+            "id": layerID + "cluster-count",
+            "type": "symbol",
+            "source": "places",
+            "layout": {
+                "text-field": "{point_count}",
+                "text-font": [
+                    "DIN Offc Pro Medium",
+                    "Arial Unicode MS Bold"
 
+                    ],
+                "text-size": 12,
+                'visibility': 'visible'
+
+            }
+
+        });
 
       // Add checkbox and label elements for the layer.
       var input = document.createElement('input');
@@ -46,8 +136,8 @@ map.on( 'load', function() {
       input.id = layerID;
       input.checked = true;
       filterGroup.appendChild(input);
-      // When the checkbox changes, update the visibility of the layer.
 
+      // When the checkbox changes, update the visibility of the layer.
 
       var label = document.createElement('label');
       label.setAttribute('for', layerID);
@@ -60,43 +150,6 @@ map.on( 'load', function() {
 
       });
 
-        var layers = [
-            [150, '#f28cb1'],
-            [20, '#f1f075'],
-            [0, '#51bbd6']
-        ];
-
-        layers.forEach(function (layer, i) {
-            map.addLayer({
-                "id": layerID + i,
-                "type": "circle",
-                "source": "places",
-                "paint": {
-                    "circle-color": layer[1],
-                    "circle-radius": 18
-                },
-                "filter": i === 0 ?
-                    [">=", "point_count", layer[0]] :
-                    ["all",
-                        [">=", "point_count", layer[0]],
-                        ["<", "point_count", layers[i - 1][0]]]
-            });
-        });
-
-        // Add a layer for the clusters' count labels
-        map.addLayer({
-            "id": "cluster-count",
-            "type": "symbol",
-            "source": "places",
-            "layout": {
-                "text-field": "{point_count}",
-                "text-font": [
-                    "DIN Offc Pro Medium",
-                    "Arial Unicode MS Bold"
-                ],
-                "text-size": 12
-            }
-        });
 
 
       map.on('click', function (e) {
@@ -114,9 +167,13 @@ map.on( 'load', function() {
         })
             .setLngLat(feature.geometry.coordinates)
             .setHTML('<h3>' + "Funding Program: " + feature.properties.FundProgram + '</h3>' +
-                '<li>' + '<b>Organization name </b>' + feature.properties.Org_Name + '</li>' +
-                '<li>' + '<b>City Served </b>' + feature.properties.City + '</li>' +
-                '<li>' + '<b>Funding Year </b>' + feature.properties.FY + '</li>'
+                '<li>' + '<b>Organization Name: </b>' + feature.properties.Org_Name + '</li>' +
+                '<li>' + '<b>City Served: </b>' + feature.properties.City + '</li>' +
+                '<li>' + '<b>Funding Year: </b>' + feature.properties.FY + '</li>' +
+                '<li>' + '<b>Target Age: </b>' + feature.properties.Targ_Age + '</li>'
+
+
+
             )
             .addTo(map);
       });
