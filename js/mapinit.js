029dbc968ddbe6e580e93f6f586a8b31e72fd3df
Alex <alexaca79@gmail.com>
2016-08-29 14:44:21 -0400

    Ui/UX

--- js/mapinit.js
+++ js/mapinit.js
@@ -57,7 +57,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYWxleGFjYTc5IiwiYSI6ImNpbzYyZGVlNzAyNjd2d2x6d
 
 var filterGroup = document.getElementById( 'filter-group' );
 var places ;
-$.getJSON("https://raw.githubusercontent.com/mcyspolihack/yo_map_mapbox/master/data/convertcsv.geojson", function(json){places=json});
+$.getJSON("https://raw.githubusercontent.com/mcyspolihack/yo_map_mapbox/gh-pages/data/convertcsv.geojson", function(json){places=json});
 
 var bounds = [
     [-56.280342 ,40.920051], // Southwest coordinates
